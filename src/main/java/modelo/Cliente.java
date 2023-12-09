76dbbec4f255357e0a897df7b3759b0d7b674e87
fotosycaptura <charsxavier@gmail.com>
2023-12-09 16:46:43 -0300

    Se agrega validarCedula

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
-
+import java.util.*;
 /**
  *
  * @author Tom
@@ -31,6 +31,30 @@ public class Cliente {
      */
     public void setCedula(String cedula) {
         this.cedula = cedula;
+        //Se pide validación de cedula. largo máximo 10
+        //Estilo xxxxxxxx-d
+        //Donde xxxxxxxx es una parte entera y d es un número de 0 a 9 o k
+        
+        if (cedula != null && cedula.length() == 10){
+            //Se debe de dividir en dos partes y realizar validación
+            StringTokenizer stk = new StringTokenizer(cedula, "-");
+            if (stk.countTokens() == 2){
+                String conversor = new String(stk.nextToken());
+                int rutNumerico = Integer.parseInt(conversor);
+                char chrDigito = stk.nextToken().charAt(0);
+                //Se valida si el digito verificaro es correcto
+                if (ValidarDigitoVerificador(rutNumerico).equals(String.valueOf(chrDigito).toUpperCase())){
+                    this.cedula = cedula;
+                }else{
+                    throw new IllegalArgumentException("Cédula de cliente inválida. Posiblemente dígito verificador no válido.");       
+                }
+            }else{
+                throw new IllegalArgumentException("Cédula de cliente inválida. Posiblemente no se incluyó el guión");   
+            }
+        }else{
+             throw new IllegalArgumentException("Cédula de cliente inválida. No posee el largo de 10 caracteres");
+        }
+
     }
 
     /**
@@ -60,4 +84,47 @@ public class Cliente {
     public void setVigente(boolean vigente) {
         this.vigente = vigente;
     }
+    
+    /**
+     * Se encarga de obtener el dígito verificador de un rut.
+     * Se utiliza para obtenerlo - generarlo - o para la comparación en un if
+     * @param rut
+     * @return 
+     */
+    private String ValidarDigitoVerificador(int rut)
+    {
+        int Digito;
+        int Contador;
+        int Multiplo;
+        int Acumulador;
+        String RutDigito;
+
+        Contador = 2;
+        Acumulador = 0;
+
+        while (rut != 0)
+        {
+            Multiplo = (rut % 10) * Contador;
+            Acumulador = Acumulador + Multiplo;
+            rut = rut / 10;
+            Contador = Contador + 1;
+            if (Contador == 8)
+            {
+                Contador = 2;
+            }
+        }
+
+        Digito = 11 - (Acumulador % 11);
+        RutDigito = Integer.toString(Digito);
+        if (Digito == 10)
+        {
+            RutDigito = "K";
+        }
+        if (Digito == 11)
+        {
+            RutDigito = "0";
+        }
+        return (RutDigito);
+    }
+
 }
