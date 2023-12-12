01896aff380d7b2420c063f2d1d09d21684dc329
xavierconx <charsxavier@gmail.com>
2023-12-12 09:02:24 -0300

    Create InterfazVehiculoAgregar.java

---
+++ src/main/java/vista/InterfazVehiculoAgregar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package vista;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import modelo.Vehiculo;
+/**
+ *
+ * @author xavier.fuentes
+ */
+public interface InterfazVehiculoAgregar {
+    public final String AGREGAR = "AGREGAR";
+    public final String VOLVER = "VOLVER";
+    
+    public JPanel getPanel();
+    public void registrarEscuchador(ActionListener escuchador);
+    public String getNombre();
+    public String getRut();
+    public void vaciarCampos();
+    public int mostrarMensaje(int tipoMensaje, String msj);
+    public void listarClientes(ArrayList<Vehiculo> vehiculos);
+}
