1e2b30f128d8a2fcd1557b9f15965da564d4b4dc
fotosycaptura <charsxavier@gmail.com>
2023-12-11 23:39:29 -0300

    Vista Vehículos
    Primera interfaz que actuará de menú

---
+++ src/main/java/controlador/ControladorVehiculos.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import modelo.Vehiculo;
+import vista.InterfazVehiculo;
+import vista.VistaVehiculos;
+
+/**
+ *
+ * @author xavie
+ */
+public class ControladorVehiculos implements ActionListener {
+    private final InterfazVehiculo vista = new VistaVehiculos();
+    private ArrayList<Vehiculo> modelo = new ArrayList<Vehiculo>();
+    
+    public ControladorVehiculos(){
+        getVista().registrarEscuchador(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        Container contenido = getVista().getPanel().getParent();
+        String command = e.getActionCommand();
+        
+        if (command.equals(getVista().LISTAR)){
+            System.out.println("HAGO CLICK AL LISTAR");
+        }
+        else if (command.equals(getVista().AGREGAR)){
+            System.out.println("HAGO CLICK AL Agregar");
+        }
+        else if (command.equals(getVista().VOLVER)) {
+            contenido.remove(getVista().getPanel());
+            contenido.getComponent(contenido.getComponentCount() - 1).setVisible(true);
+        } else {
+            throw new UnsupportedOperationException("Acción no implementada.");
+        }
+        
+        contenido.validate();
+        contenido.repaint();
+    }
+    
+    /**
+     * @return the vista
+     */
+    public InterfazVehiculo getVista() {
+        return vista;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public ArrayList<Vehiculo> getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(ArrayList<Vehiculo> modelo) {
+        this.modelo = modelo;
+    }
+    
+}
