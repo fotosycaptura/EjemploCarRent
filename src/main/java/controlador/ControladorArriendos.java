b63eccda044550136954e5de3239538773478399
fotosycaptura <charsxavier@gmail.com>
2023-12-14 10:49:23 -0300

    Update ControladorArriendos.java
    Se agregan los ArrayList de clientes y vehículos para hacerlos disponibles en el resto del los controladores de arriendos cuotas y posteriormente en el de pago

--- src/main/java/controlador/ControladorArriendos.java
+++ src/main/java/controlador/ControladorArriendos.java
@@ -7,17 +7,23 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
 import modelo.ArriendoCuota;
-import modelo.ArriendoCuota;
+import modelo.Vehiculo;
+import modelo.Cliente;
+
 import vista.InterfazArriendos;
 import vista.VistaArriendos;
 import vista.VistaArriendosArrendar;
+
 /**
  *
  * @author xavie
  */
 public class ControladorArriendos implements ActionListener{
     private InterfazArriendos vista = new VistaArriendos();
+    private ArrayList<Vehiculo> vehiculos;
+    private ArrayList<Cliente> clientes;
     private ArrayList<ArriendoCuota> modelo = new ArrayList<ArriendoCuota>();
 
     public ControladorArriendos() {
@@ -34,7 +40,9 @@ public class ControladorArriendos implements ActionListener{
         } else if (command.equals(getVista().ARRENDAR)){
             ControladorArriendosArrendar controlador = new ControladorArriendosArrendar(
                     new VistaArriendosArrendar(), 
-                    getModelo()
+                    getModelo(),
+                    getClientes(),
+                    getVehiculos()
             );
             getVista().getPanel().setVisible(false);
             contenido.add(controlador.getVista().getPanel());
@@ -77,6 +85,34 @@ public class ControladorArriendos implements ActionListener{
      */
     public void setModelo(ArrayList<ArriendoCuota> modelo) {
         this.modelo = modelo;
+    }
+
+    /**
+     * @return the vehiculos
+     */
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    /**
+     * @param vehiculos the vehiculos to set
+     */
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    /**
+     * @return the clientes
+     */
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    /**
+     * @param clientes the clientes to set
+     */
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
     }
     
 }
